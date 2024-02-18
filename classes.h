1f7fa928b340777a4a536e4c0e2fc6b390cf755a
Hyun Taek, Oh <56432035+oht505@users.noreply.github.com>
2024-02-18 14:52:15 -0800

    Add files via upload

--- classes.h
+++ classes.h
@@ -57,13 +57,15 @@ private:
     int totalNumberOfPage;    // To count the total number of pages 
     int totalNumRecords;  // To count the total number of records
     char mmpages[MAX_BLOCK_IN_MEMORY][BLOCK_SIZE]={};    // Define 3 * page(4KB) for re-hashing in main memory 
-    int pageRemaining = BLOCK_SIZE-2*POINTER_ADDRESS_SIZE-SLOT_RECORD_NUMBER_SIZE;
     int addressOfOverflowPage = -1;   // -1: no overflow page, >0: address of overflow page  
+    char overflowPage[BLOCK_SIZE] = {};
     int freeSpacePointer = 0;
     int addressOfCurrentField = FIELD_OFFSET_POINTER_SIZE;      // The address of the current field relative to the variable length record
     int addressOfCurrentOffsetField = 0;    // The address of the current offset field relative to the variable length record
-    int latestRecordOffset = BLOCK_SIZE-2*POINTER_ADDRESS_SIZE-SLOT_RECORD_NUMBER_SIZE;
-    char page[BLOCK_SIZE]={};
+    int addressOfPrevPage = -1;
+    int pageRemaining = 0;
+    int idBits = 0;
+    char page[BLOCK_SIZE] = {};
 
     // Insert new record into index
     void insertRecord(Record record, ofstream *dataFile) {
@@ -73,19 +75,24 @@ private:
         if(!readFile.is_open()){
             throw "Error Opening";
         }
-        readFile.seekg(blockDirectory[bitHash(record.id)]);
+        idBits = bitHash(record.id);
+        readFile.seekg(blockDirectory[idBits]);
         readFile.read(page,BLOCK_SIZE);
 
-        // Store numRecords 
-        memcpy(&numRecords, &page[0]+BLOCK_SIZE-2*POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
+        // Get the address of overflow page
+        memcpy(&addressOfOverflowPage, &page[0] + BLOCK_SIZE - 3 * POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
+
+        // Get numRecords 
+        memcpy(&numRecords, &page[0] + BLOCK_SIZE - 2 * POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
         //cout<<"offset:"<<blockDirectory[bitHash(record.id)]<<", numRecords:"<<numRecords<<endl;
-        memcpy(&freeSpacePointer, &page[0]+BLOCK_SIZE-POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
-        //cout<<"total_records: "<<totalNumRecords<<endl;
+
+        // Get freeSpacePointer
+        memcpy(&freeSpacePointer, &page[0] + BLOCK_SIZE-POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
+        
 
         // No records written to index yet
         if (numRecords == 0) {
-            // Initialize index with first blocks (start with 4)  
-            
+            // Initialize index with first blocks (start with 4)       
         }
 
         // Size of string (Name, Bio)
@@ -96,9 +103,35 @@ private:
         int recordLength = FIELD_OFFSET_POINTER_SIZE + (ID_FIELD_SIZE*2) + nameLength + bioLength;
         int recordRealSize = SLOT_DIRECTORY_SIZE + recordLength;
 
-        // Add record to the index in the correct block, creating a overflow block if necessary
+        // Calculate empty space in the page
+        pageRemaining = BLOCK_SIZE - SLOT_DIRECTORY_SIZE - SLOT_OVERFLOW_SIZE - freeSpacePointer - (numRecords*SLOT_DIRECTORY_SIZE);
+        //cout<<"pageRemaining:"<<pageRemaining<<endl;
 
-        // create a variable record set to store info
+        // [Overflow page] Take neccessary steps if capacity is reached:
+        if (pageRemaining < recordRealSize){
+            // Search the address of empty overflow page 
+            while (addressOfOverflowPage>0){
+                addressOfPrevPage = addressOfOverflowPage;
+                //cout<<"previous page address:"<<addressOfPrevPage<<endl;
+                overflowPage[BLOCK_SIZE]={};
+                readFile.seekg(addressOfOverflowPage);
+                readFile.read(overflowPage, sizeof(page));
+
+                memcpy(&addressOfOverflowPage, &overflowPage[0] + BLOCK_SIZE - 3 * POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
+            }
+            // Arrive at the empty overflow page
+            memcpy(&numRecords, &overflowPage[0] + BLOCK_SIZE - 2 * POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
+            memcpy(&freeSpacePointer, &overflowPage[0] + BLOCK_SIZE-POINTER_ADDRESS_SIZE, POINTER_ADDRESS_SIZE);
+            // Initialize the address of overflow page
+            addressOfOverflowPage = -1;
+            
+            // Initialize a page to store the overflow page
+            page[BLOCK_SIZE]={};
+            memcpy(page, &overflowPage, sizeof(page));
+        }
+
+        // Add record to the index in the correct block, creating a overflow block if necessary
+        // Create a variable record for storing record data
         char variableRecord[recordLength];
         
         // The address of the current field relative to the variable length record
@@ -106,7 +139,7 @@ private:
         // The address of the current offset field relative to the variable length record
         int addressOfCurrentOffsetField = 0;
 
-        // Write field offset pointer and field
+        // Write field offset pointers and fields
         // record.id
         memcpy(variableRecord + addressOfCurrentField, &record.id, sizeof(int));
         addressOfCurrentField += freeSpacePointer;
@@ -148,25 +181,40 @@ private:
         memcpy(page + freeSpacePointer, &variableRecord, recordLength);
         int recordAddress = freeSpacePointer;
         freeSpacePointer += recordLength;
-        //cout<<"offset:"<<blockDirectory[bitHash(record.id)]<<", recordAddress:"<<recordAddress<<endl;
 
+        // Update Slot Part
         // Add (offset, recordLength) to the slot directory in the page 
         memcpy(page + BLOCK_SIZE - POINTER_ADDRESS_SIZE - SLOT_RECORD_NUMBER_SIZE - SLOT_OVERFLOW_SIZE - ((numRecords+1)*8), &recordAddress, POINTER_ADDRESS_SIZE);
         memcpy(page + BLOCK_SIZE - POINTER_ADDRESS_SIZE - SLOT_RECORD_NUMBER_SIZE - SLOT_OVERFLOW_SIZE - ((numRecords+1)*8 - 4), &recordLength, RECORD_LENGTH_SIZE);
         
-        // Update free space pointer and the number of variable length records in the memory page
+        // Update free space pointer and the number of variable length records in the page
         numRecords += 1;
         totalNumRecords += 1;
         memcpy(page + BLOCK_SIZE - POINTER_ADDRESS_SIZE, &freeSpacePointer, POINTER_ADDRESS_SIZE);
         memcpy(page + BLOCK_SIZE - POINTER_ADDRESS_SIZE - SLOT_RECORD_NUMBER_SIZE, &numRecords, SLOT_RECORD_NUMBER_SIZE);
+        
+        // Calculate empty space in the current page
+        pageRemaining = BLOCK_SIZE - SLOT_DIRECTORY_SIZE - SLOT_OVERFLOW_SIZE - freeSpacePointer - (numRecords*SLOT_DIRECTORY_SIZE);
+        // Add overflow page at the end of the existing pages in the file
+        if (pageRemaining < recordRealSize){
+            // next overflow page would be (+ # of bucket and overflow pages)
+            addressOfOverflowPage = totalNumberOfPage * BLOCK_SIZE;
+            totalNumberOfPage += 1;
+        } 
+
+        // Update address of overflow page in the page
         memcpy(page + BLOCK_SIZE - 2 * POINTER_ADDRESS_SIZE - SLOT_RECORD_NUMBER_SIZE, &addressOfOverflowPage, POINTER_ADDRESS_SIZE);
 
-        (*dataFile).seekp(blockDirectory[bitHash(record.id)]);
+        // Write the page to the appropriate offset of the file
+        if (addressOfPrevPage==-1){
+            (*dataFile).seekp(blockDirectory[idBits]);
+        } else{
+            (*dataFile).seekp(addressOfPrevPage);
+        }
+
         (*dataFile).write(page, sizeof(page));
-         
-        // Take neccessary steps if capacity is reached:
-		// increase n; increase i (if necessary); place records in the new bucket that may have been originally misplaced due to a bit flip
-    
+
+		// [Re-hashing] increase n; increase i (if necessary); place records in the new bucket that may have been originally misplaced due to a bit flip
         readFile.close();
     }
 
@@ -177,6 +225,8 @@ public:
         numRecords = 0;
         nextFreeBlock = 0;
         fName = indexFileName;
+        totalNumberOfPage = n;
+        
 
         // Create your EmployeeIndex file 
         ofstream dataFile(fName, ios::out | ios::binary);
@@ -196,10 +246,6 @@ public:
             blockDirectory.push_back(nextFreeBlock*BLOCK_SIZE);    
             dataFile.write(page, sizeof(page));       
         }
-
-        // To check data safety
-        // blockDirectory.resize(8);
-        // printVector(blockDirectory);
 
         dataFile.close();
     }
@@ -231,7 +277,7 @@ public:
 
             insertRecord(record, &dataFile);
         }
-
+        cout<<"total pages:"<<totalNumberOfPage<<", total records:"<<totalNumRecords<<endl;
         // Write blockDirectory to the file
         
         
@@ -256,28 +302,22 @@ public:
         idBits = bitHash(id);
 
 
-        // Use blockDirectory to find a page from the file
-        //   * idBits can be an index of blockDirectory
-        //   * => blockDirectory[idBits]=Pointer to page
-        //   * => write directly to the file. 
-        //   
+        // [Use blockDirectory to find a page from the file]
+        //  idBits can be an index of blockDirectory
+        //  blockDirectory[idBits]=Pointer to page 
         
         
 
-        // Find a record from the page
-        //   * Read directory(offset, length)
-        //   * Use the directory pointer to locate record fields pointers
-        //   * Use record fields pointer to locate attributes of the record
+        // [Find a record from the page]
+        //  Read slot directory (numRecords, offset, length and so on)
+        //  Use the directory pointer to locate record fields pointers
+        //  Use record fields pointer to locate attributes of the record
 
-        // if: employee_ID is equal to input ID
-        //   *  
+        // [if, employee_ID is equal to input ID]
+        //  
 
-        // else: Find other records
-
-        // result.id=11442121;
-        // result.name="TesterASDF";
-        // result.bio="Idon'tknow~";
-        // result.manager_id=11433333;
+        // [else, Find other records]
+        //  
 
         return result;
     }
--- main.cpp
+++ main.cpp
@@ -20,13 +20,12 @@ int main(int argc, char* const argv[]) {
 
     Record employeeInfo;
     int employeeID = 0;
-    int result=0;
 
     // Create the index
     LinearHashIndex emp_index("EmployeeIndex");
     emp_index.createFromFile("Employee.csv");
 
-    // Loop to lookup IDs until user is ready to quit
+    //Loop to lookup IDs until user is ready to quit
     // while(true){
         
     //     cout<<"Insert employee ID: ";
@@ -38,10 +37,7 @@ int main(int argc, char* const argv[]) {
     //     if(employeeInfo.id==-1){
     //         cout<<"No record info."<<endl;
     //     } else{
-    //         cout<<"ID: "<<employeeInfo.id
-    //         <<", Name: "<<employeeInfo.name
-    //         <<", Bio: "<< employeeInfo.bio
-    //         << ", manager_id: "<<employeeInfo.manager_id<<endl;
+    //         employeeInfo.print();
     //     }
     //     cout<<"--------------------------------------";
     //     cout<<"--------------------------------------";
